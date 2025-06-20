50d3b95d1624af0144b0a65d3bfd6c348c70e008
ijedi1234 <thegreatijedi@gmail.com>
2025-06-19 20:21:45 -0500

    defined two values in evt prog set

--- MBEPacker/MBE/EXPA/Records/EXPARecordDataEventProgressSet.cs
+++ MBEPacker/MBE/EXPA/Records/EXPARecordDataEventProgressSet.cs
@@ -12,12 +12,12 @@ namespace MBEPacker.MBE.EXPA.Records
     {
 
         public int Value1 { get; set; }
-        public int Value2 { get; set; }
+        public int Day { get; set; }
         public int Value3 { get; set; }
         public int Value4 { get; set; }
         public int Value5 { get; set; }
         public int SmallValue1 { get; set; }
-        public int Value6 { get; set; }
+        public int CharID { get; set; }
         public int Value7 { get; set; }
         public string? RefID1 { get; set; }
         public string? RefID2 { get; set; }
@@ -31,12 +31,12 @@ namespace MBEPacker.MBE.EXPA.Records
         public EXPARecordDataEventProgressSet(byte[] rawRecord, List<CHNKRecordRelativeOffset> cRecords) : base(rawRecord)
         {
             Value1 = BitConverter.ToInt32(rawRecord.Skip(0x0).Take(sizeof(int)).ToArray());
-            Value2 = BitConverter.ToInt32(rawRecord.Skip(0x4).Take(sizeof(int)).ToArray());
+            Day = BitConverter.ToInt32(rawRecord.Skip(0x4).Take(sizeof(int)).ToArray());
             Value3 = BitConverter.ToInt32(rawRecord.Skip(0x8).Take(sizeof(int)).ToArray());
             Value4 = BitConverter.ToInt32(rawRecord.Skip(0xC).Take(sizeof(int)).ToArray());
             Value5 = BitConverter.ToInt32(rawRecord.Skip(0x10).Take(sizeof(int)).ToArray());
             SmallValue1 = BitConverter.ToInt32(rawRecord.Skip(0x14).Take(sizeof(int)).ToArray());
-            Value6 = BitConverter.ToInt32(rawRecord.Skip(0x18).Take(sizeof(int)).ToArray());
+            CharID = BitConverter.ToInt32(rawRecord.Skip(0x18).Take(sizeof(int)).ToArray());
             Value7 = BitConverter.ToInt32(rawRecord.Skip(0x1C).Take(sizeof(int)).ToArray());
             Value8 = BitConverter.ToInt32(rawRecord.Skip(0x30).Take(sizeof(int)).ToArray());
             Value9 = BitConverter.ToInt32(rawRecord.Skip(0x34).Take(sizeof(int)).ToArray());
@@ -55,12 +55,12 @@ namespace MBEPacker.MBE.EXPA.Records
         public EXPARecordDataEventProgressSet(JsonObject json) : base(json)
         {
             Value1 = json["Value1"].AsValue().GetValue<int>();
-            Value2 = json["Value2"].AsValue().GetValue<int>();
+            Day = json["Day"].AsValue().GetValue<int>();
             Value3 = json["Value3"].AsValue().GetValue<int>();
             Value4 = json["Value4"].AsValue().GetValue<int>();
             Value5 = json["Value5"].AsValue().GetValue<int>();
             SmallValue1 = json["SmallValue1"].AsValue().GetValue<int>();
-            Value6 = json["Value6"].AsValue().GetValue<int>();
+            CharID = json["CharID"].AsValue().GetValue<int>();
             Value7 = json["Value7"].AsValue().GetValue<int>();
             if (json["RefID1"] == null) { RefID1 = null; } else { RefID1 = (string)json["RefID1"]; }
             if (json["RefID2"] == null) { RefID2 = null; } else { RefID2 = (string)json["RefID2"]; }
@@ -74,12 +74,12 @@ namespace MBEPacker.MBE.EXPA.Records
         {
             JsonObject json = new JsonObject();
             json["Value1"] = Value1;
-            json["Value2"] = Value2;
+            json["Day"] = Day;
             json["Value3"] = Value3;
             json["Value4"] = Value4;
             json["Value5"] = Value5;
             json["SmallValue1"] = SmallValue1;
-            json["Value6"] = Value6;
+            json["CharID"] = CharID;
             json["Value7"] = Value7;
             json["RefID1"] = RefID1;
             json["RefID2"] = RefID2;
@@ -97,12 +97,12 @@ namespace MBEPacker.MBE.EXPA.Records
             byte[] empty8 = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
             List<byte> finalList = new List<byte>();
             finalList.AddRange(BitConverter.GetBytes(Value1));
-            finalList.AddRange(BitConverter.GetBytes(Value2));
+            finalList.AddRange(BitConverter.GetBytes(Day));
             finalList.AddRange(BitConverter.GetBytes(Value3));
             finalList.AddRange(BitConverter.GetBytes(Value4));
             finalList.AddRange(BitConverter.GetBytes(Value5));
             finalList.AddRange(BitConverter.GetBytes(SmallValue1));
-            finalList.AddRange(BitConverter.GetBytes(Value6));
+            finalList.AddRange(BitConverter.GetBytes(CharID));
             finalList.AddRange(BitConverter.GetBytes(Value7));
             finalList.AddRange(empty8);
             finalList.AddRange(empty8);

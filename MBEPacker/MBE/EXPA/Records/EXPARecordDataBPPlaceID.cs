aefad1b5d8a69edb82a6878213618ef979537bda
ijedi1234 <thegreatijedi@gmail.com>
2025-06-23 21:19:16 -0500

    updates to placeid props

--- MBEPacker/MBE/EXPA/Records/EXPARecordDataBPPlaceID.cs
+++ MBEPacker/MBE/EXPA/Records/EXPARecordDataBPPlaceID.cs
@@ -12,48 +12,48 @@ namespace MBEPacker.MBE.EXPA.Records
     public class EXPARecordDataBPPlaceID : EXPARecord
     {
 
-        public int Value1 { get; set; }
+        public int PlaceID { get; set; }
         public int Value2 { get; set; }
         public int Value3 { get; set; }
-        public int Value4 { get; set; }
-        public int Value5 { get; set; }
-        public int Value6 { get; set; }
-        public int Value7 { get; set; }
+        public int LotteryID { get; set; }
+        public int TurnBeginMove { get; set; }
+        public int TurnsReinforce { get; set; }
+        public int DefendID { get; set; }
 
         public EXPARecordDataBPPlaceID() { }
 
         public EXPARecordDataBPPlaceID(byte[] rawRecord) : base(rawRecord)
         {
-            Value1 = BitConverter.ToInt32(rawRecord.Skip(0x0).Take(sizeof(int)).ToArray());
+            PlaceID = BitConverter.ToInt32(rawRecord.Skip(0x0).Take(sizeof(int)).ToArray());
             Value2 = BitConverter.ToInt32(rawRecord.Skip(0x4).Take(sizeof(int)).ToArray());
             Value3 = BitConverter.ToInt32(rawRecord.Skip(0x8).Take(sizeof(int)).ToArray());
-            Value4 = BitConverter.ToInt32(rawRecord.Skip(0xC).Take(sizeof(int)).ToArray());
-            Value5 = BitConverter.ToInt32(rawRecord.Skip(0x10).Take(sizeof(int)).ToArray());
-            Value6 = BitConverter.ToInt32(rawRecord.Skip(0x14).Take(sizeof(int)).ToArray());
-            Value7 = BitConverter.ToInt32(rawRecord.Skip(0x18).Take(sizeof(int)).ToArray());
+            LotteryID = BitConverter.ToInt32(rawRecord.Skip(0xC).Take(sizeof(int)).ToArray());
+            TurnBeginMove = BitConverter.ToInt32(rawRecord.Skip(0x10).Take(sizeof(int)).ToArray());
+            TurnsReinforce = BitConverter.ToInt32(rawRecord.Skip(0x14).Take(sizeof(int)).ToArray());
+            DefendID = BitConverter.ToInt32(rawRecord.Skip(0x18).Take(sizeof(int)).ToArray());
         }
 
         public EXPARecordDataBPPlaceID(JsonObject json) : base(json)
         {
-            Value1 = json["Value1"].AsValue().GetValue<int>();
+            PlaceID = json["PlaceID"].AsValue().GetValue<int>();
             Value2 = json["Value2"].AsValue().GetValue<int>();
             Value3 = json["Value3"].AsValue().GetValue<int>();
-            Value4 = json["Value4"].AsValue().GetValue<int>();
-            Value5 = json["Value5"].AsValue().GetValue<int>();
-            Value6 = json["Value6"].AsValue().GetValue<int>();
-            Value7 = json["Value7"].AsValue().GetValue<int>();
+            LotteryID = json["LotteryID"].AsValue().GetValue<int>();
+            TurnBeginMove = json["TurnBeginMove"].AsValue().GetValue<int>();
+            TurnsReinforce = json["TurnsReinforce"].AsValue().GetValue<int>();
+            DefendID = json["DefendID"].AsValue().GetValue<int>();
         }
 
         public override JsonObject GetJson()
         {
             JsonObject json = new JsonObject();
-            json["Value1"] = Value1;
+            json["PlaceID"] = PlaceID;
             json["Value2"] = Value2;
             json["Value3"] = Value3;
-            json["Value4"] = Value4;
-            json["Value5"] = Value5;
-            json["Value6"] = Value6;
-            json["Value7"] = Value7;
+            json["LotteryID"] = LotteryID;
+            json["TurnBeginMove"] = TurnBeginMove;
+            json["TurnsReinforce"] = TurnsReinforce;
+            json["DefendID"] = DefendID;
             return json;
         }
 
@@ -61,13 +61,13 @@ namespace MBEPacker.MBE.EXPA.Records
         {
             byte[] ending = new byte[] { 0xCC, 0xCC, 0xCC, 0xCC };
             List<byte> finalList = new List<byte>();
-            finalList.AddRange(BitConverter.GetBytes(Value1));
+            finalList.AddRange(BitConverter.GetBytes(PlaceID));
             finalList.AddRange(BitConverter.GetBytes(Value2));
             finalList.AddRange(BitConverter.GetBytes(Value3));
-            finalList.AddRange(BitConverter.GetBytes(Value4));
-            finalList.AddRange(BitConverter.GetBytes(Value5));
-            finalList.AddRange(BitConverter.GetBytes(Value6));
-            finalList.AddRange(BitConverter.GetBytes(Value7));
+            finalList.AddRange(BitConverter.GetBytes(LotteryID));
+            finalList.AddRange(BitConverter.GetBytes(TurnBeginMove));
+            finalList.AddRange(BitConverter.GetBytes(TurnsReinforce));
+            finalList.AddRange(BitConverter.GetBytes(DefendID));
             finalList.AddRange(ending);
             return finalList.ToArray();
         }

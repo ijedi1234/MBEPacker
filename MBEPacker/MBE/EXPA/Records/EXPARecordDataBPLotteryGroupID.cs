711dc254290d432496df5466e163686ed4becdb8
ijedi1234 <thegreatijedi@gmail.com>
2025-06-23 22:07:19 -0500

    lottery update

--- MBEPacker/MBE/EXPA/Records/EXPARecordDataBPLotteryGroupID.cs
+++ MBEPacker/MBE/EXPA/Records/EXPARecordDataBPLotteryGroupID.cs
@@ -11,9 +11,9 @@ namespace MBEPacker.MBE.EXPA.Records
     public class EXPARecordDataBPLotteryGroupID : EXPARecord
     {
 
-        public int Value1 { get; set; }
-        public int Value2 { get; set; }
-        public int Value3 { get; set; }
+        public int Index { get; set; }
+        public int PlaceID { get; set; }
+        public int UnitID { get; set; }
         public int Value4 { get; set; }
         public int Value5 { get; set; }
 
@@ -21,18 +21,18 @@ namespace MBEPacker.MBE.EXPA.Records
 
         public EXPARecordDataBPLotteryGroupID(byte[] rawRecord) : base(rawRecord)
         {
-            Value1 = BitConverter.ToInt32(rawRecord.Skip(0x0).Take(sizeof(int)).ToArray());
-            Value2 = BitConverter.ToInt32(rawRecord.Skip(0x4).Take(sizeof(int)).ToArray());
-            Value3 = BitConverter.ToInt32(rawRecord.Skip(0x8).Take(sizeof(int)).ToArray());
+            Index = BitConverter.ToInt32(rawRecord.Skip(0x0).Take(sizeof(int)).ToArray());
+            PlaceID = BitConverter.ToInt32(rawRecord.Skip(0x4).Take(sizeof(int)).ToArray());
+            UnitID = BitConverter.ToInt32(rawRecord.Skip(0x8).Take(sizeof(int)).ToArray());
             Value4 = BitConverter.ToInt32(rawRecord.Skip(0xC).Take(sizeof(int)).ToArray());
             Value5 = BitConverter.ToInt32(rawRecord.Skip(0x10).Take(sizeof(int)).ToArray());
         }
 
         public EXPARecordDataBPLotteryGroupID(JsonObject json) : base(json)
         {
-            Value1 = json["Value1"].AsValue().GetValue<int>();
-            Value2 = json["Value2"].AsValue().GetValue<int>();
-            Value3 = json["Value3"].AsValue().GetValue<int>();
+            Index = json["Index"].AsValue().GetValue<int>();
+            PlaceID = json["PlaceID"].AsValue().GetValue<int>();
+            UnitID = json["UnitID"].AsValue().GetValue<int>();
             Value4 = json["Value4"].AsValue().GetValue<int>();
             Value5 = json["Value5"].AsValue().GetValue<int>();
         }
@@ -40,9 +40,9 @@ namespace MBEPacker.MBE.EXPA.Records
         public override JsonObject GetJson()
         {
             JsonObject json = new JsonObject();
-            json["Value1"] = Value1;
-            json["Value2"] = Value2;
-            json["Value3"] = Value3;
+            json["Index"] = Index;
+            json["PlaceID"] = PlaceID;
+            json["UnitID"] = UnitID;
             json["Value4"] = Value4;
             json["Value5"] = Value5;
             return json;
@@ -52,9 +52,9 @@ namespace MBEPacker.MBE.EXPA.Records
         {
             byte[] ending = new byte[] { 0xCC, 0xCC, 0xCC, 0xCC };
             List<byte> finalList = new List<byte>();
-            finalList.AddRange(BitConverter.GetBytes(Value1));
-            finalList.AddRange(BitConverter.GetBytes(Value2));
-            finalList.AddRange(BitConverter.GetBytes(Value3));
+            finalList.AddRange(BitConverter.GetBytes(Index));
+            finalList.AddRange(BitConverter.GetBytes(PlaceID));
+            finalList.AddRange(BitConverter.GetBytes(UnitID));
             finalList.AddRange(BitConverter.GetBytes(Value4));
             finalList.AddRange(BitConverter.GetBytes(Value5));
             finalList.AddRange(ending);
